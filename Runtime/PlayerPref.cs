7300a6a281c7b408801a6310d7856b2b3e269897
Dastan Dzhumabekov <dastan.wm@gmail.com>
2022-09-08 02:30:06 +0600

    Added encryption

---
+++ Runtime/PlayerPref.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+
+namespace Dythervin.PersistentData
+{
+    public abstract class PlayerPref
+    {
+        public string key;
+
+        protected PlayerPref(string key)
+        {
+            this.key = key;
+        }
+    }
+
+    public abstract class PlayerPref<T> : PlayerPref
+    {
+        private T _value;
+        private bool _isLoaded;
+
+        protected PlayerPref(string key, T defaultValue = default) : base(key)
+        {
+            _value = defaultValue;
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (!_isLoaded)
+                {
+                    if (IsSaved)
+                    {
+                        _value = PersistentValue;
+                    }
+
+                    _isLoaded = true;
+                }
+
+                return _value;
+            }
+
+            set
+            {
+                PersistentValue = value;
+                _value = value;
+                _isLoaded = true;
+            }
+        }
+
+        public bool IsSaved => PlayerPrefs.HasKey(key);
+
+        protected abstract T PersistentValue { get; set; }
+    }
+
+    public class PlayerPrefInt : PlayerPref<int>
+    {
+        public PlayerPrefInt(string key, int defaultValue = default) : base(key, defaultValue) { }
+
+        protected override int PersistentValue
+        {
+            get => PlayerPrefs.GetInt(key);
+            set => PlayerPrefs.SetInt(key, value);
+        }
+    }
+
+    public class PlayerPrefFloat : PlayerPref<float>
+    {
+        public PlayerPrefFloat(string key, float defaultValue = default) : base(key, defaultValue) { }
+
+        protected override float PersistentValue
+        {
+            get => PlayerPrefs.GetFloat(key);
+            set => PlayerPrefs.SetFloat(key, value);
+        }
+    }
+
+    public class PlayerPrefString : PlayerPref<string>
+    {
+        public PlayerPrefString(string key, string defaultValue = default) : base(key, defaultValue) { }
+
+        protected override string PersistentValue
+        {
+            get => PlayerPrefs.GetString(key);
+            set => PlayerPrefs.SetString(key, value);
+        }
+    }
+
+    public class PlayerPrefBool : PlayerPref<bool>
+    {
+        public PlayerPrefBool(string key, bool defaultValue = default) : base(key, defaultValue) { }
+
+        protected override bool PersistentValue
+        {
+            get => PlayerPrefs.GetInt(key) == 1;
+            set => PlayerPrefs.SetInt(key, value ? 1 : 0);
+        }
+    }
+}
--- Runtime/PrefContainer.Static.cs
+++ Runtime/PrefContainer.Static.cs
@@ -1,4 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text;
+#if UNITY_EDITOR
+using Dythervin.Core.Editor;
+#endif
 using Newtonsoft.Json;
 using UnityEditor;
 using UnityEngine;
@@ -8,6 +13,11 @@ namespace Dythervin.PersistentData
     public partial class PrefContainer
     {
         private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto };
+
+        private static PlayerPrefInt _version = new PlayerPrefInt("CryptVer");
+#if UNITY_EDITOR
+        private static EditorPrefBool _enabled = new EditorPrefBool("PrefsCrypt");
+#endif
 
 #if UNITY_EDITOR
         internal static PrefContainer GetAtProject(string path)
@@ -24,22 +34,45 @@ namespace Dythervin.PersistentData
 
         internal static PrefContainer GetAt(string path)
         {
-            PrefContainer pref = File.Exists(path)
-                ? JsonConvert.DeserializeObject<PrefContainer>(File.ReadAllText(path), Settings)
-                  ?? new PrefContainer()
-                : new PrefContainer();
+            PrefContainer pref = File.Exists(path) ? Deserialize(path) : new PrefContainer();
+
             pref.Path = path;
             return pref;
         }
 
+        private static string GetString(string path)
+        {
+            switch (_version.Value)
+            {
+                case 0: return File.ReadAllText(path);
+                case 1: return Encoding.UTF8.GetString(File.ReadAllBytes(path));
+                default: throw new ArgumentOutOfRangeException();
+            }
+        }
+
         private static void Save(PrefContainer prefs)
         {
-            File.WriteAllText(prefs.Path, JsonConvert.SerializeObject(prefs, Formatting.Indented, Settings));
-#if UNITY_EDITOR
             string dirPath = System.IO.Path.GetDirectoryName(prefs.Path);
             if (!Directory.Exists(dirPath))
                 Directory.CreateDirectory(dirPath);
+
+            string json = JsonConvert.SerializeObject(prefs, Formatting.Indented, Settings);
+#if UNITY_EDITOR
+            if (!_enabled.Value)
+            {
+                File.WriteAllText(prefs.Path, json);
+            }
 #endif
+            File.WriteAllBytes(prefs.Path, Encoding.UTF8.GetBytes(json));
+            _version.Value = 1;
+            //To ensure
+            PlayerPrefs.Save();
+        }
+
+        private static PrefContainer Deserialize(string path)
+        {
+            return JsonConvert.DeserializeObject<PrefContainer>(GetString(path), Settings)
+                   ?? new PrefContainer();
         }
     }
 }
